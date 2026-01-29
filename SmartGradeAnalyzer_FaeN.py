85e305588ee8fefb1e3d65074e73cd23259449f6
fae <faen769@deltalearns.ca>
2026-01-29 11:43:17 -0800

    Create python file and comment out code; planning how to code the grade averager.

---
+++ SmartGradeAnalyzer_FaeN.py
@@ -0,0 +1,24 @@
+"""
+Smart Grade Analyzer Project
+This is my first project as a Master of Infotech!
+This will be used to calculate a students average grade given their scores in a course.
+User will be able to input scores between 0-100, and can let the code know when they are done to begin calculations.
+"""
+# import extensions (eg. regex)
+
+
+# define variables
+
+
+
+# greet user, ask for the number of courses they'd like to calculate for (or ask them to begin listing their grades)
+
+
+# save each input into the dictionary or list
+
+
+
+# check for any invalid characters using regex, tell them to re-state that grade properly
+
+
+# once user states they are done listing, calculate the average using the number of courses they inputted into the system
