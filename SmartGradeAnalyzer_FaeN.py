606c5c5922be377c92c734fd6409f889a2c34af1
fae <faen769@deltalearns.ca>
2026-01-30 09:38:32 -0800

    Create function to collect users grades
    A loop will begin where the user will continuously input grades, after 2 grades are inputted, the user will be prompted to calculate the average.
    
    right now, the loop does not work, "while not done" has an issue where it will not occur due to the values.

--- SmartGradeAnalyzer_FaeN.py
+++ SmartGradeAnalyzer_FaeN.py
@@ -6,6 +6,7 @@ User will be able to input scores between 0-100, and can let the code know when 
 """
 # import modules required (eg. regex)
 import re
+import math
 
 # define variables
 courses = []
@@ -20,17 +21,35 @@ C = list(range(60, 67))
 Cminus = list(range(50, 60))
 F = list(range(0, 50))
 
-# greet user, ask for the number of courses they'd like to calculate for (or ask them to begin listing their grades)
+# begin the calculating, allow for user to begin inputting values,
+def collect():
+    print("Please input one grade at a time and using your grade percentage! (e.g. 96, 75)\n\nWhen you are done, type DONE")
+    while not done:
+        for loop in range():
+            grade = int(input("Course Grade:   ").strip().isnumeric())
+            courses.append(grade) # save each input into the list
 
-
+            if num <= 2:
+                input("Anything else? [Y / N]\n").strip().isalpha()
+                if "y": 
+                    done = True
+                if "n":
+                    done = False
+                else:
+                    print("Not a valid input, please indicate Y or N!")
+                    return
+            break
+        break
+    while done:
+        print("not done yet!")
 
 # check for any invalid characters using regex, tell them to re-state that grade properly
 
 
 # once user states they are done listing, calculate the average using the number of courses they inputted into the system
 
-
 # obtain users name and then call upon a function to begin
 name = input("Hey user! Before we start calculating, what is your name?\n\n").strip().capitalize()
 print(f"Thanks {name}, let's begin now.")
 
+collect()
