b1fd298630b9ad8c8484d7a930be5145f7d88b02
fae <faen769@deltalearns.ca>
2026-02-02 14:47:48 -0800

    Obtain letter grade
    Use ranges to figure out where the average sits in terms of letter grades. If average falls within a specific range, it falls under that letter grade.

--- SmartGradeAnalyzer_FaeN.py
+++ SmartGradeAnalyzer_FaeN.py
@@ -10,22 +10,38 @@ courses = []
 done = False
 num = len(courses) # collect the number of courses through the number of grades inputted.
 
-# state letter grades based on numerical percentages
-A = list(range(86, 101))
-B = list(range(73, 86))
-Cplus = list(range(67, 73))
-C = list(range(60, 67))
-Cminus = list(range(50, 60))
-F = list(range(0, 50))
+# # state letter grades based on numerical percentages
+# Aye = range(86, 101)
+# Bee = range(73, 86)
+# Cplus = range(67, 73)
+# Cee = range(60, 67)
+# Cminus = range(50, 60)
+# Eff = range(0, 50)
 
 # once user states they are done listing, calculate the average using the number of courses they inputted into the system
 def calculation():
-    print("ur in the calculating function!!!!")
 
     # collect each number within the list of courses, and add them all together
     totalsum = sum(courses)
-    avg = totalsum/len(courses)
-    print(f"Out of your {len(courses)} grades, your grade average is {avg}")
+    avg = round(totalsum/len(courses))
+
+    # with the average, check which letter grade the average corresponds with
+    if avg in range(86, 101):
+        lettergrade = "A"
+    elif avg in range(73, 86):
+        lettergrade = "B"
+    elif avg in range(67, 73):
+        lettergrade = "C+"
+    elif avg in range (60, 67):
+        lettergrade = "C"
+    elif avg in range(50, 60):
+        lettergrade = "C-"
+    else:
+        lettergrade = "F"
+    # state the highest and lowest grade the user received
+
+    # print the results; average, highest/lowest, and letter grade
+    print(f"Out of your {len(courses)} grades: your grade average is {avg}, which is a {lettergrade}")
 
 
 # begin the calculating, allow for user to begin inputting values,
@@ -34,15 +50,13 @@ def collect(done):
     
     # loop that goes on while the users grade input is still ongoing.
     while not done:
-        # break loop when user states they are done
 
-
-        
         # let user know how many courses theyve inputted so far.
         print(f"Number of courses inputted: {len(courses)}")
         userinput = input("Course Grade:   ").strip() # variable of each grade
 
         # if there are 2 or more inputs and user states they are done, move on to the next step.
+        # break loop when user states they are done
         if userinput == "done":
             if len(courses) >= 2: 
                 done = True
